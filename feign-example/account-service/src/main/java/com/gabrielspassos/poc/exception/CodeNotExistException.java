2e354c05bdfa270d0e24de6c405dde0c59404a10
gabrielSpassos <gabrielsantos45725@gmail.com>
2018-01-18 15:06:19 -0200

    removido pasta feign antiga

--- feign-example/account-service/src/main/java/com/gabrielspassos/poc/exception/CodeNotExistException.java
+++
@@ -1,6 +0,0 @@
-package com.gabrielspassos.poc.exception;
-
-public class CodeNotExistException extends Exception {
-    public CodeNotExistException() {
-    }
-}
--- feign-example/person-service/src/main/java/com/gabrielspassos/poc/exception/IdNotExistException.java
+++
@@ -1,6 +0,0 @@
-package com.gabrielspassos.poc.exception;
-
-public class IdNotExistException extends Exception {
-    public IdNotExistException() {
-    }
-}
